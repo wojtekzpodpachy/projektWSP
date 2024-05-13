e33e12a384a9567e5d8323f9abff8ac1540eb42b
wssw <wssw@DESKTOP-7QM7HGR>
2024-05-13 15:56:54 +0200

    BEZ MASY KULKI RUCH

--- Logika/BallLogic.cs
+++ Logika/BallLogic.cs
@@ -10,7 +10,7 @@ public class BallLogic
     private const int CanvasHeight = 457;
     private const int OffsetX = 0;
     private const int OffsetY = 0;
-    private const double Margin = 0.5; 
+    private const double Margin = 0.5; // Smaller margin for accuracy
     private Random random = new Random();
 
     public Ball Move(Ball ball)
@@ -71,7 +71,7 @@ public class BallLogic
         }
     }
 
-    private void ResolveCollision(Ball ball1, Ball ball2)
+    /*private void ResolveCollision(Ball ball1, Ball ball2)
     {
         // Oblicz różnice w położeniu (od lewego górnego rogu)
         double deltaX = ball2.X - ball1.X;
@@ -82,7 +82,7 @@ public class BallLogic
         double overlap = BallDiameter - distance + Margin;
         if (overlap > 0)
         {
-            
+            // Oblicz znormalizowaną różnicę
             double normX = deltaX / distance;
             double normY = deltaY / distance;
 
@@ -95,14 +95,42 @@ public class BallLogic
             // Oblicz względną prędkość w kierunku kolizji
             double relVelocityX = ball1.VelocityX - ball2.VelocityX;
             double relVelocityY = ball1.VelocityY - ball2.VelocityY;
-            double dotProduct = 1;
+            double dotProduct = relVelocityX * normX + relVelocityY * normY;
 
-            // Wymień prędkości 
+            // Wymień prędkości (zasada zachowania pędu)
             double impulse = 2 * dotProduct / (ball1.Mass + ball2.Mass);
             ball1.VelocityX -= impulse * ball2.Mass * normX;
             ball1.VelocityY -= impulse * ball2.Mass * normY;
             ball2.VelocityX += impulse * ball1.Mass * normX;
             ball2.VelocityY += impulse * ball1.Mass * normY;
+        }
+    }*/
+    private void ResolveCollision(Ball ball1, Ball ball2)
+    {
+        // Oblicz różnice w położeniu (od lewego górnego rogu)
+        double deltaX = ball2.X - ball1.X;
+        double deltaY = ball2.Y - ball1.Y;
+        double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+        // Zapobieganie przyklejeniu przez zapewnienie minimalnego odstępu
+        double overlap = BallDiameter - distance + Margin;
+        if (overlap > 0)
+        {
+            double normX = deltaX / distance;
+            double normY = deltaY / distance;
+
+            ball1.X -= normX * overlap / 2;
+            ball1.Y -= normY * overlap / 2;
+            ball2.X += normX * overlap / 2;
+            ball2.Y += normY * overlap / 2;
+
+            double velocityComponent1 = (ball1.VelocityX * normX + ball1.VelocityY * normY);
+            double velocityComponent2 = (ball2.VelocityX * normX + ball2.VelocityY * normY);
+            ball1.VelocityX -= 2 * velocityComponent1 * normX;
+            ball1.VelocityY -= 2 * velocityComponent1 * normY;
+            ball2.VelocityX -= 2 * velocityComponent2 * normX;
+            ball2.VelocityY -= 2 * velocityComponent2 * normY;
+           
         }
     }
 
